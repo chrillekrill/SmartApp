0e5d4e8086de51368d8807f945fa140b615284f0
Christoffer K <henricson21@hotmail.com>
2022-10-04 20:40:38 +0200

    new stuff with new stuff added

--- SmartApp/MVVM/ViewModels/KitchenViewModel.cs
+++ SmartApp/MVVM/ViewModels/KitchenViewModel.cs
@@ -1,17 +1,130 @@
-﻿using System;
+﻿using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Shared;
+using SmartApp.MVVM.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Threading;
 
 namespace SmartApp.MVVM.ViewModels
 {
     internal class KitchenViewModel
     {
-        public string Title { get; set; } = "Kitchen and dining";
-        public string Temperature { get; set; } = "24";
-        public string TemperatureScale { get; set; } = "°C";
-        public string Humidity { get; set; } = "33";
-        public string HumidityScale { get; set; } = "%";
+        private DispatcherTimer timer;
+        private ObservableCollection<DeviceItem> _deviceItems;
+        private List<DeviceItem> _tempList;
+        private readonly RegistryManager registryManager = RegistryManager.CreateFromConnectionString("HostName=iothubv73.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=QkPsCR+vmNjexd1ggpjtGzppwC+Udq0LymJL3jgUxIQ=");
+
+        public KitchenViewModel()
+        {
+            _tempList = new List<DeviceItem>();
+            _deviceItems = new ObservableCollection<DeviceItem>();
+            PopulateDeviceItemsAsync().ConfigureAwait(false);
+            SetInterval(TimeSpan.FromSeconds(3));
+        }
+
+
+        public string Title { get; set; } = "Kitchen";
+        public string Temperature { get; set; } = "23 °C";
+        public string Humidity { get; set; } = "34 %";
+        public IEnumerable<DeviceItem> DeviceItems => _deviceItems;
+
+
+
+        private void SetInterval(TimeSpan interval)
+        {
+            timer = new DispatcherTimer()
+            {
+                Interval = interval
+            };
+
+            timer.Tick += new EventHandler(timer_tick);
+            timer.Start();
+        }
+
+        private async void timer_tick(object sender, EventArgs e)
+        {
+            await PopulateDeviceItemsAsync();
+            await UpdateDeviceItemsAsync();
+        }
+
+
+        private async Task UpdateDeviceItemsAsync()
+        {
+            _tempList.Clear();
+
+            foreach (var item in _deviceItems)
+            {
+                var device = await registryManager.GetDeviceAsync(item.DeviceId);
+                if (device == null)
+                    _tempList.Add(item);
+            }
+
+            foreach (var item in _tempList)
+            {
+                _deviceItems.Remove(item);
+            }
+        }
+
+        private async Task PopulateDeviceItemsAsync()
+        {
+            //var result = registryManager.CreateQuery("select * from devices where location = 'kitchen'");
+            var result = registryManager.CreateQuery("select * from devices where properties.reported.location = 'kitchen'");
+
+            if (result.HasMoreResults)
+            {
+                foreach (Twin twin in await result.GetNextAsTwinAsync())
+                {
+                    var device = _deviceItems.FirstOrDefault(x => x.DeviceId == twin.DeviceId);
+
+                    if (device == null)
+                    {
+                        device = new DeviceItem
+                        {
+                            DeviceId = twin.DeviceId,
+                        };
+
+                        try { device.DeviceName = twin.Properties.Reported["deviceName"]; }
+                        catch { device.DeviceName = device.DeviceId; }
+                        try { device.DeviceType = twin.Properties.Reported["deviceType"]; }
+                        catch { }
+
+                        switch (device.DeviceType.ToLower())
+                        {
+                            case "fan":
+                                device.IconActive = "\uf863";
+                                device.IconInActive = "\uf863";
+                                device.StateActive = "ON";
+                                device.StateInActive = "OFF";
+                                break;
+
+                            case "light":
+                                device.IconActive = "\uf672";
+                                device.IconInActive = "\uf0eb";
+                                device.StateActive = "ON";
+                                device.StateInActive = "OFF";
+                                break;
+
+                            default:
+                                device.IconActive = "\uf2db";
+                                device.IconInActive = "\uf2db";
+                                device.StateActive = "ENABLE";
+                                device.StateInActive = "DISABLE";
+                                break;
+                        }
+
+                        _deviceItems.Add(device);
+                    }
+                    else { }
+                }
+            }
+            else
+            {
+                _deviceItems.Clear();
+            }
+        }
     }
 }
