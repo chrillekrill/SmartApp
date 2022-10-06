ddbdeb57701a67b4a7d0ceff60ee4e26cb949e28
Christoffer K <henricson21@hotmail.com>
2022-10-06 17:38:15 +0200

    added device project and small fixes

--- SmartApp/MainWindow.xaml.cs
+++ SmartApp/MainWindow.xaml.cs
@@ -17,7 +17,7 @@ namespace SmartApp
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    /// </summary>w§§§§§§§§
     public partial class MainWindow : Window
     {
         public MainWindow()
